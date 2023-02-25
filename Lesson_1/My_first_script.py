582a056f20dcad30531922707f6955b2281852f7
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-02-25 10:26:25 +0200

    add
    add1

---
+++ Lesson_1/My_first_script.py
@@ -0,0 +1,11 @@
+"""
+This is Python
+"""
+# print  - команда для вывода информации в консоль
+print('My first commit')
+
+# input - команда для получения ввода пользователя из консоли
+name = input('What is your name? ')
+
+# f - format, форматируемая стока, которая дает возможность в себя включать переменную
+print(f'Hello {name}!')
