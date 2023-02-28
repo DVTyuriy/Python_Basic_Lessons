fce5e7d8a9d35df91de94257de0d4c39fef9ed1f
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-03-01 01:55:27 +0200

    fix2

---
+++ Lesson_3/Home_work_for_lesson_3.py
@@ -0,0 +1,30 @@
+
+input_text = input('Введите любой текст, где есть пробелы, запятые и много разного), не скупитесь на знаки .,-:;?!) \n') # ввод предложения для обработки
+# input_text = 'HelLo,.- mY nAmE iS: YuRii;! I. aM PrograMMing on C++) WhAt abOuT yoU? :)  ' # строка для теста, нужно просто раскоментить 3 строки, это первая
+
+input_text_ed = input_text.lower() # превращение предложения в нижний регистр
+input_text_ed = input_text_ed.replace('.', '') # Удаление точек в предложении
+input_text_ed = input_text_ed.replace(',', '') # Удаление запятых в предложении
+input_text_ed = input_text_ed.replace(')', '') # Удаление скобок в предложении
+input_text_ed = input_text_ed.replace('-', '') # Удаление тире в предложении
+input_text_ed = input_text_ed.replace(':', '') # Удаление двоеточия в предложении
+input_text_ed = input_text_ed.replace(';', '') # Удаление точек с запятыми в предложении
+input_text_ed = input_text_ed.replace('?', '') # Удаление знаков вопроса в предложении
+input_text_ed = input_text_ed.replace('!', '') # Удаление знаков восклицания в предложении
+
+find_word = input('Какое слово хотите заменить?\n') # какое слово надо заменить
+
+#find_word = 'C++' # строка для теста, нужно просто раскоментить, это вторая
+
+print(f'Ваше слово {find_word} находится на позиции: {input_text.find(find_word)}!') # вывод позиции слова в первоначальной формулировке
+
+find_word_ed = find_word.lower() # это нужно, чтобы менять слово уже в измененном тексте
+
+rep_word = input('На какое слово хотите заменить?\n') # на какое слово хотите заменить
+
+# rep_word = 'Python' # для теста # строка для теста, нужно просто раскоментить, это крайняя
+
+final_text = input_text_ed.replace(find_word_ed, rep_word) # выполняется замена слова
+
+print(f'Мы получили вот такой результат:') # вывод результата
+print(final_text.rstrip()) # вывод результата с удалением справа пробелов
