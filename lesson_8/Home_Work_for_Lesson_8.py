7c9b623e8654fb35c0368b3cd6d6f6d51ac557c3
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-03-23 01:18:03 +0200

    Main 733

--- lesson_8/Home_Work_for_Lesson_8.py
+++ lesson_8/Home_Work_for_Lesson_8.py
@@ -7,25 +7,26 @@ def printed_for(input_list: list, comment: str):
     input_list = list()
 
 
-def latest(latest_list: list, comment: str):
-
+def latest(latest_tuple: tuple, comment: str):
+    latest_list = list(latest_tuple)
     latest_list.reverse()
     printed_for(latest_list, comment)
 
 
-def longest_short(longest_list: list, rev: bool, comment):
+def longest_short(longest_tuple: tuple, rev: bool, comment):
+    longest_list = list(longest_tuple)
     longest_list.sort(key=len, reverse=rev)
     printed_for(longest_list, comment)
 
 
 if __name__ == '__main__':
     # об`являємо змінну як список
+    contain_tuple = tuple()
     contain_list = list()
-    ls = list()
     while True:
         # інпут нотатків від юзера, або команди на обробку нотатків
-        input_string = input('Введіть текст для збереження в нотатки\n'
-                             'Або ви ще маєте вибір\n'
+        input_word = input('Введіть \n'
+                             'add -      для збереження в нотатки\n'
                              'earliest - щоб вивести нотатки на экран від найранішої до найпізнішої:\n'
                              'latest -   щоб вивести нотатки на экран від найпізнішої до найранішої:\n'
                              'longest -  щоб вивести нотатки на экран від найдовшої до найкоротшої:\n'
@@ -37,36 +38,35 @@ if __name__ == '__main__':
         #                 'well, anyways']
 
         # перевірка, якщо юзер хоче вийти
-        if input_string.lower() == 'exit':
+        if input_word.lower() == 'exit':
             exit()
 
         # перевірка, якщо юзер хоче відсортувати від найранішої до найпізнішої
-        if input_string.lower() == 'earliest':
-            printed_for(contain_list, 'Від найранішої до найпізнішої:')
+        if input_word.lower() == 'earliest':
+            printed_for(contain_tuple, 'Від найранішої до найпізнішої:')
 
         # перевірка, якщо юзер хоче відсортувати від найпізнішої до найранішої
-        elif input_string.lower() == 'latest':
-            latest(contain_list, 'Від найпізнішої до найранішої:')
+        elif input_word.lower() == 'latest':
+            latest(contain_tuple, 'Від найпізнішої до найранішої:')
 
         # перевірка, якщо юзер хоче відсортувати від найдовшої до найкоротшої
-        elif input_string.lower() == 'longest':
-            ls = contain_list
+        elif input_word.lower() == 'longest':
             # показуємо, що ми хочемо сортувати в залежності від довжини елемента списку та в зворотньому напрямку
-            longest_short(ls, True, 'Від найдовшої до найкоротшої:')
+            longest_short(contain_tuple, True, 'Від найдовшої до найкоротшої:')
 
         # перевірка, якщо юзер хоче відсортувати від найкоротшої до найдовшої
-        elif input_string.lower() == 'shortest':
-            ls = contain_list
+        elif input_word.lower() == 'shortest':
             # показуємо, що ми хочемо сортувати в залежності від довжини елемента списку
-            longest_short(ls, False, 'Від найкоротшої до найдовшої:')
+            longest_short(contain_tuple, False, 'Від найкоротшої до найдовшої:')
 
         # перевірка, якщо юзер не ввів нічого
-        elif input_string == '':
+        elif input_word == '':
             print('Ви нічого не ввели, а ми нічого і не записали')
             print()
 
         # Якщо нічого не підійшло, то додаємо введений текст до нотатків
-        else:
-            contain_list.append(input_string)
+        elif input_word == 'add':
+            input_list = input()
+            contain_list.append(input_list)
+            contain_tuple = tuple(contain_list)
             print('Так, це ми записали до нотатків')
-            ls = contain_list
