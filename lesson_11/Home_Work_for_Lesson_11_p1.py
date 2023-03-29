1081da61e69886fae514418dd7efc5b67ed53eb3
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-03-29 14:47:17 +0300

    main fix2

--- lesson_11/Home_Work_for_Lesson_11_p1.py
+++ lesson_11/Home_Work_for_Lesson_11_p1.py
@@ -1,8 +1,7 @@
 COMMAND = [
     'Введіть:',
     'open -     відкрити та прочитати всі записи',
-    'save -     зберегти ведений текст до нотаток',
-    'add -      для збереження в нотатки',
+    'add -      для додавання нотаток',
     'earliest - щоб вивести нотатки на экран від найранішої до найпізнішої:',
     'latest -   щоб вивести нотатки на экран від найпізнішої до найранішої:',
     'longest -  щоб вивести нотатки на экран від найдовшої до найкоротшої:',
@@ -12,25 +11,46 @@ COMMAND = [
 
 
 def open_f() -> list:
+    """
+    Функія відкриття файлу з нотатками
+    :return: вертає прочитану інфу з файла
+    """
     lines = list()
-    try:
-        with open(name_save_open(), 'r', encoding='utf-8') as f:
-            for line in f.readlines():
-                lines.append(line.strip())
+    # відкриваємо файл та зчитуємо інформацію
+    while True:
+        try:
+            with open(name_save_open(), 'r', encoding='utf-8') as f:
+                for line in f.readlines():
+                    lines.append(line.strip())
+                print('Файл було прочитано')
+                return lines
+        except Exception:
+            print('Дідко, не можу прочитати цей файл, давай прочитаємо інший, або створимо нові нотатки')
+            print()
+            # вертаємо пустий список, якщо не змогли прочитати файл
             return lines
-    except Exception:
-        print('Файла з нотатками я не знайшов, давай створимо новий')
 
 
 def save_f(tuple_to_save: tuple):
+    """
+    Функція, яка зберігає інформацію до файлу при виході з програми
+    :param tuple_to_save: Кортеж з нотатками
+    :return: нічого не повертаємо, виходимо з програми
+    """
     list_to_save = list(tuple_to_save)
+    # зберігаємо введену/зчитану інформацію до файлу
     with open(name_save_open(), 'w', encoding='utf-8') as write_f:
         text = '\n'.join(list_to_save)
         write_f.write(text)
+    print('Інформацію було збережено')
 
 
 def name_save_open() -> str:
-    return input('Ведіть назву файла: ') + '.txt'
+    """
+    Функція для вводу назви файлу при відкритті/збереженні
+    :return: повертаємо назву файла
+    """
+    return input('Ведіть назву файла: ')
 
 
 def earliest(earliest_tuple: tuple, comment: str):
@@ -77,12 +97,9 @@ def printed_for(input_list_print: list, comment: str):
     :return: функція нічого не повертає, а лише виводить на екран інформацію зі списку
     """
     n = input_num()
-    if n > len(input_list_print):
-        n = len(input_list_print)
     print(comment)
-    print(len(input_list_print))
-    for i in range(n):
-        print(input_list_print[i])
+    for note in input_list_print[:n]:
+        print(note)
     print('=' * 10, 'New operation', '=' * 10)
 
 
@@ -94,6 +111,9 @@ def input_num() -> int:
     while True:
         try:
             k = int(input('Скільки нотатків ви хочете побачити: '))
+            if k <= 0:
+                print("Не можу сгенерувати від'ємну або нульову нотатків")
+                continue
             return k
         except Exception:
             print('Трясся, я не можу сприйняти це як число')
@@ -104,35 +124,32 @@ def print_ln():
     Функція для виведення на екран команд для взаємодії
     :return: функція нічого не повертає, а лише виводить на екран інформацію зі списку
     """
-    for j in range(len(COMMAND)):
-        print(COMMAND[j])
+    for j in COMMAND:
+        print(j)
 
 
 if __name__ == '__main__':
     # об`являємо змінні
     contain_tuple = tuple()
     contain_list = list()
-    file_list = list()
-    p = 0
     while True:
         # виводимо інфу  для взаємодії с користувачем
         print_ln()
         # інпут нотатків від юзера, або команди на обробку нотатків
         input_word = input()
 
-        # Якщо було введено 'add', то додаємо наступний введений текст до нотатків
+        # Якщо було введено 'open', відкриваємо файл та читаємо його, записуємо в кортеж
         if input_word == 'open':
             contain_list += open_f()
             contain_tuple = tuple(contain_list)
 
-        elif input_word == 'save':
-            save_f(contain_tuple)
-
+        # Якщо було введено 'add', то додаємо наступний введений текст до нотатків
         elif input_word == 'add':
             print('Введіть текст для додавання в нотатки')
             input_list = input()
             # якщо нічого не було введено, нічого не додаємо до нотатків
-            if input_list == '':
+            if not input_list:
+                pass
                 print('Ви нічого не ввели, а ми нічого не записали:)')
                 print()
             else:
@@ -145,21 +162,25 @@ if __name__ == '__main__':
         # перевірка, якщо юзер хоче відсортувати від найранішої до найпізнішої
         elif input_word.lower() == 'earliest':
             earliest(contain_tuple, 'Від найранішої до найпізнішої:')
+            print()
 
         # перевірка, якщо юзер хоче відсортувати від найпізнішої до найранішої
         elif input_word.lower() == 'latest':
             latest(contain_tuple, 'Від найпізнішої до найранішої:')
+            print()
 
         # перевірка, якщо юзер хоче відсортувати від найдовшої до найкоротшої
         elif input_word.lower() == 'longest':
             longest_short(contain_tuple, True, 'Від найдовшої до найкоротшої:')
+            print()
 
         # перевірка, якщо юзер хоче відсортувати від найкоротшої до найдовшої
         elif input_word.lower() == 'shortest':
             # показуємо, що ми хочемо сортувати в залежності від довжини елемента списку
             longest_short(contain_tuple, False, 'Від найкоротшої до найдовшої:')
+            print()
 
-        # перевірка, якщо юзер хоче вийти
+        # перевірка, якщо юзер хоче вийти та зберегти данні
         elif input_word.lower() == 'exit':
             save_f(contain_tuple)
             exit()
