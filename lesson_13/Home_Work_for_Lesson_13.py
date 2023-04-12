faa0a9ae5667bbf0af6d8b424800edc50b7e5dbe
DVTyuriy <95183732+DVTyuriy@users.noreply.github.com>
2023-04-12 19:24:14 +0300

    322332
    32232323

--- lesson_13/Home_Work_for_Lesson_13.py
+++ lesson_13/Home_Work_for_Lesson_13.py
@@ -1,48 +1,72 @@
 from time import sleep
 from random import randint, choices
 
+
 # об`являємо клас
 class Cat:
     # конструктор класса или метод инициализации
     # вызывается автоматически при создании объекта этого класса
     def __init__(
-            self, name: str, gender: str, age: int,
-            poroda: str, myau_min: int, preferred_food: set
+            self, name: str, sex: str, age: int,
+            poroda: str, myau_min: int, play_min: int, pr_food: set
     ):
         """
         Рождение объекта класса/конструктор/инициализация
         :param name:
         """
         self.name = name
-        self.gender = gender
+        self.sex = sex
         self.age = age
         self.poroda = poroda
         self.myau_min = myau_min
-        self.preferred_food = preferred_food
-        self.cat_wont_sleep = False 
+        self.play_min = play_min
+        self.pr_food = pr_food
+        self.cat_wont_sleep = True
         self.cat_walk = False
         self.hungry = False
 
     def __str__(self) -> str:
-        return f'{self.poroda.title()}, {self.age} лет по кличке {self.name}'
+        return f'{self.poroda.title()}, {self.age} років за ім`ям {self.name}'
 
-    def bark(self):
-        print(f'{self.name} Мяукає!')
+    def myau(self):
+        print(f'{self.name} Мявкає!')
 
-    def play(self):
-        if self.play_hours == 0:
-            print(f'{self.name} не гуляє')
+    def playmin(self):
+        if self.play_min == 0:
+            print(f'{self.name} Зовсім не погралася')
+            cat.playmin()
         else:
-            print(f'{self.name} играет {self.play_hours} часов!')
-            print('Ждемо...')
-            # sleep(self.play_hours)
-            self.bark()
-            print('Гавкает потому что довольна!')
-        print(f'{self.name} проголодалась')
+            print(f'{self.name} грає {self.play_min} хвилин!')
+            if self.play_min > 10:
+                print('Можемо розслабитись')
+                sleep(self.play_min)
+                self.cat_wont_sleep = False
+                self.hungry = True
+                self.cat_walk = False
+            else:
+                print('Знову потрібно з котом погратись')
+            self.myau()
+            print('Мявчить, тому що все гуд')
+        print(f'{self.name} зголодніла, завжди готова поїсти')
+
+    def sleeptime():
+        i = randint(0, 20)
+        if i == 0:
+            print('Кішка не виспалась')
+            self.cat_wont_sleep = False
+        self.cat_wont_sleep = True
+
+    def myaumin(self):
+        if self.myau_min == 0:
+            print(f'{self.name} щось вона давно не мявкала')
+        else:
+            print(f'{self.name} знову мявкає вже {self.myau_min} хвилин!')
+            print('Мявчить, значить все гуд!')
+        print(f'{self.name} зголодніла')
         self.hungry = True
 
     def eat(self, suggested_food: str):
-        if suggested_food in self.preferred_food:
+        if suggested_food in self.pr_food:
             print(f'{self.name} кушает {suggested_food}')
             self.hungry = False
         else:
@@ -50,25 +74,25 @@ class Cat:
 
 
 if __name__ == '__main__':
-    kisa= Cat('Кіса', 'ж', 4, 'Шотландка', randint(2, 6), {'риба', 'м`ясо'})
-    Nyusha = Cat('', 'M', 5, 'йоркский терьер', 0, {'корм', 'яблоки'})
-    napas = Cat('Напас', 'M', 14, 'такса', 1, {'корм', 'сало', 'борщ'})
-    jessy = Cat('Джесси', 'Ж', 9, 'канекорса', 4, {'мясо', 'каша'})
+    Kisa = Cat('Кіса', 'ж', 1, 'Шотландка', randint(0, 3), randint(0, 20), {'риба', 'м`ясо', 'корм'})
+    Nyusha = Cat('Нюша', 'ж', 3, 'Балинезійська', randint(0, 3), randint(0, 20), {'корм', 'солодощі', 'супродукти'})
+    Belka = Cat('Білка', 'ж', 9, 'Бенгальська', randint(0, 3), randint(0, 20), {'корм', 'кістки', 'суп'})
+    Lotos = Cat('Лотос', 'м', 8, 'Бомбейська', randint(0, 3), randint(0, 20), {'супродукти', 'каша', 'кисломолочка'})
+    Snow = Cat('Сноу', 'м', 6, 'Бразильська ', randint(0, 3), randint(0, 20), {'кисломолочка', 'яйця', 'каша'})
+    Night = Cat('Найт', 'м', 5, 'БРагамаффин', randint(0, 3), randint(0, 20), {'мясо', 'каша', 'сир', 'яйця'})
 
-    potential_food = ['рыба', 'мясо', 'корм', 'яблоки', 'сало', 'борщ', 'каша']
-    print(bobik, type(bobik))
-    print(rocky, type(rocky))
-    hungry_Cats = list()
-    for Cat in [bobik, rocky, napas, jessy]:
-        print('=' * 20, f'\nОбычный день {Cat}:')
-        Cat.bark()
-        Cat.play()
-        # choices(list, k=3) k - сколько раз выбрать случайный элемент
-        for food in choices(potential_food, k=2):
-            Cat.eat(food)
-        if Cat.hungry:
-            hungry_Cats.append(Cat)
+    po_food = ['риба', 'м`ясо', 'корм', 'кістки', 'супродукти', 'кисломолочка', 'борщ', 'каша', 'яйця']
+    hungry_сats = list()
+    for cat in [Kisa, Nyusha, Belka, Lotos, Snow, Night]:
+        print(f'Звичайний день, кота на ім`я {cat}:')
+        cat.myau()
+        cat.myaumin()
+        cat.playmin()
+        for food in choices(po_food, k=2):
+            cat.eat(food)
+        if cat.hungry:
+            hungry_сats.append(cat)
 
-    if hungry_Cats:
-        print(f'Найдены {len(hungry_Cats)} голодная(ые) собака(и), срочно покормите их!')
-        print('Вот их имена:', ', '.join([Cat.name for Cat in hungry_Cats]))
+    if hungry_сats:
+        print(f'Найдены {len(hungry_сats)} голодная(ые) собака(и), срочно покормите их!')
+        print('Вот их имена:', ', '.join([Cat.name for Cat in hungry_сats]))
